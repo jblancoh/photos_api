3cb1aee3869fd236dcda87880bfa8f51a1a4fe13
jblancoh <zlatanjon@hotmail.com>
2017-01-09 16:13:09 -0600

    update photocontroller

--- controllers/photoController.js
+++ controllers/photoController.js
@@ -8,12 +8,11 @@ module.exports = function(server) {
   server.get('/photos', (req, res, next) => {
     PhotoModel.find({}, function(err, photos){
       Autor.populate(photos, {path: "user"}, function(err, photos){
-        Comment.populate(photos,{ path: "comment"}, function(err,photos){
+        Comment.populate(photos, { path: "comment"}, function(err,photos){
           helpers.success(res, next, photos)
           return next()
         })
       })
-
     })
   })
   server.get('/photo/:id', (req, res, next) =>{
@@ -24,12 +23,11 @@ module.exports = function(server) {
       return next()
     }
     PhotoModel.findOne({_id: req.params.id}, function(err, photo){
-        Comment.populate(photo,{ path: "comment"}, function(err,photo){
+      Comment.populate(photo, { path: "comment"}, function(err,photo){
         if (photo === null){
           helpers.failure(res, next, 'La foto especificada no puede ser encontrada', 404)
           return next()
         }
-        console.log(photo.comment);
         helpers.success(res, next, photo)
         return next()
       })
